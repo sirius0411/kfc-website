7c35289c06cf299ad1276615e28670fbba54ab99
siriusK <sirius0411@gmail.com>
2021-02-10 02:00:59 +0800

    seo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+const publicPath = __webpack_public_path__
+console.log(`current public path ${publicPath}`)
+
 const motto = document.getElementById('motto')
 
 motto.addEventListener('mouseenter', () => {
-    motto.src = 'assets/kfc-true.png'
+    motto.src = publicPath + 'assets/kfc-true.png'
 })
 
 motto.addEventListener('mouseleave', () => {
-    motto.src = 'assets/kfc.png'
+    motto.src = publicPath + 'assets/kfc.png'
 })
 
 const logo = document.getElementById('logo')
 logo.addEventListener('click', () => {
-    let audio = new Audio('assets/kfc.mp3')
+    let audio = new Audio(publicPath + 'assets/kfc.mp3')
     audio.currentTime = 0
     audio.play()
 })
